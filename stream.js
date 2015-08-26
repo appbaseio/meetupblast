5bce860b2532857f1ba3c16d363e8525f58297d2
Siddharth Kothari <sids.aquarius@gmail.com>
2015-08-25 19:57:23 -0700

    initial commit: working stream code

---
+++ stream.js
@@ -0,0 +1,20 @@
+
+var streamingClient = appbase.newClient({
+    url: 'https://61ONSqYR2:8820fb93-72e7-4dbf-a2a9-4b378f0197c9@scalr.api.appbase.io',
+    appname: 'meetuprsvp',
+});
+
+streamingClient.streamSearch({
+      type: 'meetup',
+      body: {
+        size: 50,
+        query: {
+            match_all: {}
+        }
+      },
+}).on('data', function(res) {
+      // client would emit "data" event every time there is a document update.
+      console.log(res)
+}).on('error', function(err) {
+      console.log(err)
+})

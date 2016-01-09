9e2fe325800e3103ff91f5cf1fc88d632f2d48e8
Farhan Chauhan <farhan687@gmail.com>
2016-01-09 10:57:56 +0530

    Add debug true in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,20 @@ var gulp = require('gulp');
 var source = require("vinyl-source-stream");
 var reactify = require('reactify');
 
-gulp.task('browserify', function(){
-  var b = browserify({entries:['src/app.js']});
-  b.transform(reactify); // use the reactify transform
-  return b.bundle()
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./dist'));
+gulp.task('browserify', function() {
+	var b = browserify({
+		entries: ['src/app.js'],
+		debug: true
+	});
+	b.transform(reactify); // use the reactify transform
+	return b.bundle()
+		.pipe(source('main.js'))
+		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('watch',function(){
+gulp.task('watch', function() {
 	gulp.watch('src/*.js', ['browserify']);
 	gulp.watch('src/*.jsx', ['browserify']);
 });
 
-gulp.task('default',['watch', 'browserify']);
+gulp.task('default', ['watch', 'browserify']);
